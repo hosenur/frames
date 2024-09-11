3fca4351fa85d77b12a90edfa2f68095c6b56794
hosenur <hosenur.dev@gmail.com>
2024-09-11 20:52:35 +0530

    previewed owned images in dashboard

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -2,12 +2,11 @@ import Image from '#models/image'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
-    async index({ auth, request, response, inertia }: HttpContext) {
+    async index({ auth, inertia }: HttpContext) {
         const user = await auth.user
         const ID = user?.id
         const images = await Image.findManyBy('owner', ID)
         console.log(images)
-
         return inertia.render('dashboard/index', { images })
     }
 }
--- inertia/pages/dashboard/index.tsx
+++ inertia/pages/dashboard/index.tsx
@@ -9,6 +9,7 @@ export default function Home(props: { version: number }) {
   const { data, setData, post, progress } = useForm<{ image: File | null }>({
     image: null,
   })
+  console.log(props)
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     console.log(data)
@@ -27,6 +28,11 @@ export default function Home(props: { version: number }) {
           />
           <Button type="submit">Upload</Button>
         </Form>
+      </div>
+      <div>
+        {props.images.map((image) => (
+          <img src={`http://localhost:3333/images/_/${image.name}`} alt={image.name} />
+        ))}
       </div>
     </AppLayout>
   )
